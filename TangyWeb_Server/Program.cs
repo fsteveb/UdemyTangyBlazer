9469168feee5b18a03e027ea338b8d09fa6d5888
Steve Brailsford <fsteveb@gmail.com>
2023-03-18 13:05:43 -0400

    Changes through assignment 2

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -24,6 +24,10 @@ namespace TangyWeb_Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
             app.UseHttpsRedirection();
 
